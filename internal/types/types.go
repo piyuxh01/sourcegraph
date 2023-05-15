316ef02a1e4cc7d5221cb41af903bca55e94ebf1
Erzhan Torokulov <erzhan.torokulov@gmail.com>
2023-05-15 12:15:56 +0600

    fixup! refactor: update structure of ping event

--- internal/types/types.go
+++ internal/types/types.go
@@ -1011,6 +1011,9 @@ type CodyAggregatedEvent struct {
 	InvalidDay          int32
 }
 
+// NOTE: DO NOT alter this struct without making a symmetric change
+// RepoMetadataAggregatedStats represents the total and unique number of
+// events for repo metadata usage related events over the current day, week, month.
 type RepoMetadataAggregatedStats struct {
 	IsEnabled bool
 	Daily     *RepoMetadataAggregatedStatsPeriod
@@ -1019,11 +1022,11 @@ type RepoMetadataAggregatedStats struct {
 }
 
 type RepoMetadataAggregatedStatsPeriod struct {
-	StartTime time.Time
+	StartTime          time.Time
 	CreateRepoMetadata *EventStats
 	UpdateRepoMetadata *EventStats
 	DeleteRepoMetadata *EventStats
-	SearchFilterUsage *EventStats
+	SearchFilterUsage  *EventStats
 }
 
 type EventStats struct {
